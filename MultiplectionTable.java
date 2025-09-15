618938d9a7d1c843ba2f2013cf377c3e3febaea8
Imrajes <drajesh14082002@gmail.com>
2025-09-15 11:30:38 +0530

    edited Multiplication Table

--- MultiplectionTable.java
+++ MultiplectionTable.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class MultiplectionTable {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to print its multiplication table: ");
+        int num = sc.nextInt();
+        multiplicationTable(num);
+    }
+    public static void multiplicationTable(int n){
+        int i =1;
+        while(i<=10){
+            System.out.println(n + " x " + i + " = " + (n*i));
+            i++;
+    }
+}
 }
